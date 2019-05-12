03564b4390acf3b8e3c1fc1a5ccc2b121b67966e
Gene Strickland <thecheeskake@gmail.com>
2019-05-12 10:29:06 -0700

    created files for project

---
+++ best_fit.cpp
@@ -0,0 +1,11 @@
+#include "project2.h"
+
+void best_fit(const std::vector<double>& items, std::vector<int>& assignment, std::vector<double>& free_space)
+{
+
+}
+
+void best_fit_decreasing(const std::vector<double>& items, std::vector<int>& assignment, std::vector<double>& free_space)
+{
+
+}
